78bb7f8119d1c9c4556475e2da22905e2b788598
Nathaniel Abrante <nathaniel.lacerda@gmail.com>
2026-05-21 11:08:04 -0300

    feat: add MantineProvider and React Query to main application
    - Introduced postcss configuration for Mantine preset.
    - Removed obsolete skills-lock.json file.
    - Updated main.tsx to include MantineProvider and QueryClientProvider for enhanced state management and styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@mantine/core/styles.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
